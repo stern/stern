2623d8d886d65c5569e9ab9ad541c92f2756ecf3
Guillaume Fillon <kokaz@users.noreply.github.com>
2016-10-10 12:29:40 +0200

    Add distribution process (#2)

---
+++ scripts/create-release-notes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cat << EOF
+## Changelog
+
+TODO
+
+## Checksums
+
+- stern_linux_amd64:
+  - sha256: \`$(awk '{print $1}' "$DIR/../artifacts/latest/linux_amd64/SHA256SUMS")\`
+- stern_darwin_amd64:
+  - sha256: \`$(awk '{print $1}' "$DIR/../artifacts/latest/darwin_amd64/SHA256SUMS")\`
+
+EOF
+
