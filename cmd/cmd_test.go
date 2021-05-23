8b3f807a5b2039022fc23cf6ec374f62cd7f2b01
Kazuki Suda <kazuki.suda@gmail.com>
2021-05-23 11:48:14 +0900

    Add tests for cmd.NewSternCmd

---
+++ cmd/cmd_test.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"strings"
+	"testing"
+
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+func TestSternCommand(t *testing.T) {
+	tests := []struct {
+		name string
+		args []string
+		out  string
+	}{
+		{
+			"Output version info with --version",
+			[]string{"--version"},
+			"version: dev",
+		},
+		{
+			"Output completion code for bash with --completion=bash",
+			[]string{"--completion=bash"},
+			"complete -o default -F __start_stern stern",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			streams, _, out, _ := genericclioptions.NewTestIOStreams()
+			cmd := NewSternCmd(streams)
+			cmd.SetArgs(tt.args)
+
+			if err := cmd.Execute(); err != nil {
+				t.Fatal(err)
+			}
+
+			if !strings.Contains(out.String(), tt.out) {
+				t.Errorf("expected to contain %s, but actual %s", tt.out, out.String())
+			}
+		})
+	}
+}
