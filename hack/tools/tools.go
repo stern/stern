f403cf387c4ac99e5028f4e1e5193d90a0d112b4
Kazuki Suda <230185+superbrothers@users.noreply.github.com>
2022-05-21 22:21:36 +0900

    Use Go 1.18 (#182)
    * Use Go 1.18
    
    * Update golangci-lint
    
    Some linters of golangci-lint do not yet support Go 1.18, and this
    version disables them.
    
    * Install tools by go install

--- hack/tools/tools.go
+++
@@ -1,8 +0,0 @@
-// +build tools
-package main
-
-import (
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/goreleaser/goreleaser"
-	_ "sigs.k8s.io/krew/cmd/validate-krew-manifest"
-)
