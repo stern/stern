ed714601b93f2d9c9347eed1a7dd677d3d4cf562
Takashi Kusumi <tkusumi@zlab.co.jp>
2023-02-23 08:48:14 +0900

    Add unit test for options.sternConfig()

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -2,12 +2,18 @@ package cmd
 
 import (
 	"bytes"
+	"reflect"
+	"regexp"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/fatih/color"
 	"github.com/stern/stern/stern"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/utils/pointer"
 )
 
 func TestSternCommand(t *testing.T) {
@@ -323,6 +329,317 @@ func TestOptionsGenerateTemplate(t *testing.T) {
 			}
 			if want, got := tt.want, buf.String(); want != got {
 				t.Errorf("want %v, but got %v", want, got)
+			}
+		})
+	}
+}
+
+func TestOptionsSternConfig(t *testing.T) {
+	streams := genericclioptions.NewTestIOStreamsDiscard()
+
+	local, _ := time.LoadLocation("Local")
+	utc, _ := time.LoadLocation("UTC")
+	labelSelector, _ := labels.Parse("l=sel")
+	fieldSelector, _ := fields.ParseSelector("f=field")
+
+	re := regexp.MustCompile
+
+	defaultConfig := func() *stern.Config {
+		return &stern.Config{
+			KubeConfig:            "",
+			ContextName:           "",
+			Namespaces:            []string{},
+			PodQuery:              re(""),
+			ExcludePodQuery:       nil,
+			Timestamps:            false,
+			Location:              local,
+			ContainerQuery:        re(".*"),
+			ExcludeContainerQuery: nil,
+			ContainerStates:       []stern.ContainerState{stern.ALL_STATES},
+			Exclude:               nil,
+			Include:               nil,
+			InitContainers:        true,
+			EphemeralContainers:   true,
+			Since:                 48 * time.Hour,
+			AllNamespaces:         false,
+			LabelSelector:         labels.Everything(),
+			FieldSelector:         fields.Everything(),
+			TailLines:             nil,
+			Template:              nil, // ignore when comparing
+			Follow:                true,
+			Resource:              "",
+			OnlyLogLines:          false,
+			MaxLogRequests:        50,
+
+			Out:    streams.Out,
+			ErrOut: streams.ErrOut,
+		}
+	}
+
+	tests := []struct {
+		name      string
+		o         *options
+		want      *stern.Config
+		wantError bool
+	}{
+		{
+			"default",
+			NewOptions(streams),
+			defaultConfig(),
+			false,
+		},
+		{
+			"change all options",
+			func() *options {
+				o := NewOptions(streams)
+				o.kubeConfig = "kubeconfig1"
+				o.context = "context1"
+				o.namespaces = []string{"ns1", "ns2"}
+				o.podQuery = "query1"
+				o.excludePod = []string{"exp1", "exp2"}
+				o.timestamps = true
+				o.timezone = "UTC" // Location
+				o.container = "container1"
+				o.excludeContainer = []string{"exc1", "exc2"}
+				o.containerStates = []string{"running", "terminated"}
+				o.exclude = []string{"ex1", "ex2"}
+				o.include = []string{"in1", "in2"}
+				o.initContainers = false
+				o.ephemeralContainers = false
+				o.since = 1 * time.Hour
+				o.allNamespaces = true
+				o.selector = "l=sel"
+				o.fieldSelector = "f=field"
+				o.tail = 10
+				o.noFollow = true // Follow = false
+				o.maxLogRequests = 30
+				o.resource = "res1"
+				o.onlyLogLines = true
+
+				return o
+			}(),
+			func() *stern.Config {
+				c := defaultConfig()
+				c.KubeConfig = "kubeconfig1"
+				c.ContextName = "context1"
+				c.Namespaces = []string{"ns1", "ns2"}
+				c.PodQuery = re("query1")
+				c.ExcludePodQuery = []*regexp.Regexp{re("exp1"), re("exp2")}
+				c.Timestamps = true
+				c.Location = utc
+				c.ContainerQuery = re("container1")
+				c.ExcludeContainerQuery = []*regexp.Regexp{re("exc1"), re("exc2")}
+				c.ContainerStates = []stern.ContainerState{stern.RUNNING, stern.TERMINATED}
+				c.Exclude = []*regexp.Regexp{re("ex1"), re("ex2")}
+				c.Include = []*regexp.Regexp{re("in1"), re("in2")}
+				c.InitContainers = false
+				c.EphemeralContainers = false
+				c.Since = 1 * time.Hour
+				c.AllNamespaces = true
+				c.LabelSelector = labelSelector
+				c.FieldSelector = fieldSelector
+				c.TailLines = pointer.Int64(10)
+				c.Follow = false
+				c.Resource = "res1"
+				c.OnlyLogLines = true
+				c.MaxLogRequests = 30
+
+				return c
+			}(),
+			false,
+		},
+		{
+			"noFollow has the different default",
+			func() *options {
+				o := NewOptions(streams)
+				o.noFollow = true // Follow = false
+
+				return o
+			}(),
+			func() *stern.Config {
+				c := defaultConfig()
+				c.Follow = false
+				c.MaxLogRequests = 5 // default of noFollow
+
+				return c
+			}(),
+			false,
+		},
+		{
+			"nil should be allowed",
+			func() *options {
+				o := NewOptions(streams)
+				o.excludePod = nil
+				o.excludeContainer = nil
+				o.containerStates = nil
+				o.namespaces = nil
+				o.exclude = nil
+				o.include = nil
+
+				return o
+			}(),
+			func() *stern.Config {
+				c := defaultConfig()
+				c.ContainerStates = []stern.ContainerState{}
+
+				return c
+			}(),
+			false,
+		},
+		{
+			"error podQuery",
+			func() *options {
+				o := NewOptions(streams)
+				o.podQuery = "[invalid"
+
+				return o
+			}(),
+			nil,
+			true,
+		},
+		{
+			"error excludePod",
+			func() *options {
+				o := NewOptions(streams)
+				o.excludePod = []string{"exp1", "[invalid"}
+
+				return o
+			}(),
+			nil,
+			true,
+		},
+		{
+			"error container",
+			func() *options {
+				o := NewOptions(streams)
+				o.container = "[invalid"
+
+				return o
+			}(),
+			nil,
+			true,
+		},
+		{
+			"error excludeContainer",
+			func() *options {
+				o := NewOptions(streams)
+				o.excludeContainer = []string{"exc1", "[invalid"}
+
+				return o
+			}(),
+			nil,
+			true,
+		},
+		{
+			"error exclude",
+			func() *options {
+				o := NewOptions(streams)
+				o.exclude = []string{"ex1", "[invalid"}
+
+				return o
+			}(),
+			nil,
+			true,
+		},
+		{
+			"error include",
+			func() *options {
+				o := NewOptions(streams)
+				o.include = []string{"in1", "[invalid"}
+
+				return o
+			}(),
+			nil,
+			true,
+		},
+		{
+			"error containerStates",
+			func() *options {
+				o := NewOptions(streams)
+				o.containerStates = []string{"running", "invalid"}
+
+				return o
+			}(),
+			nil,
+			true,
+		},
+		{
+			"error selector",
+			func() *options {
+				o := NewOptions(streams)
+				o.selector = "-"
+
+				return o
+			}(),
+			nil,
+			true,
+		},
+		{
+			"error fieldSelector",
+			func() *options {
+				o := NewOptions(streams)
+				o.fieldSelector = "-"
+
+				return o
+			}(),
+			nil,
+			true,
+		},
+		{
+			"error color",
+			func() *options {
+				o := NewOptions(streams)
+				o.color = "invalid"
+
+				return o
+			}(),
+			nil,
+			true,
+		},
+		{
+			"error output",
+			func() *options {
+				o := NewOptions(streams)
+				o.output = "invalid"
+
+				return o
+			}(),
+			nil,
+			true,
+		},
+		{
+			"error timezone",
+			func() *options {
+				o := NewOptions(streams)
+				o.timezone = "invalid"
+
+				return o
+			}(),
+			nil,
+			true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.o.sternConfig()
+			if tt.wantError {
+				if err == nil {
+					t.Errorf("expected error, but got no error")
+				}
+				return
+			}
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+				return
+			}
+
+			// We skip the template as it is difficult to check
+			// and is tested in TestOptionsGenerateTemplate().
+			got.Template = nil
+
+			if !reflect.DeepEqual(tt.want, got) {
+				t.Errorf("want %+v, but got %+v", tt.want, got)
 			}
 		})
 	}
